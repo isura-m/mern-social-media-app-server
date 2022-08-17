9afeeacfd6b65ab50e645d33c9ae1d8e736ce9b3
isura <isuradev@icloud.com>
2022-08-17 07:10:38 +0530

    add controllers

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from "express";
+import cors from "cors";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import cors from "cors";
+import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb,", extended: true }));
+app.use("/posts", postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extend: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 const CONNECTION_URL =
@@ -14,13 +17,10 @@ const CONNECTION_URL =
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
-    app.listen(PORT, () => console.log("Server running on port: " + PORT))
+    app.listen(PORT, () => console.log("Server is running on port " + PORT))
   )
   .catch((error) => console.log(error.message));
 
-// mongoose.set("useFindAndModify", false);
+// mongoose.set("useFindAndModify", false); // this is deprecated!
